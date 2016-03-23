0ce2f8a8fabc40eb461bbafb45e01cd73bb134e8
yvan <yvanscher@gmail.com>
2016-03-23 10:50:07 -0400

    changed multiline comment

--- sh/hades_tunnels/tunnel_functions.sh
+++ sh/hades_tunnels/tunnel_functions.sh
@@ -3,7 +3,7 @@
 # creates a tunnel to hades given inputs
 # return the result
 create_hades_tunnel () {
-    sshpass -p $1 autossh -M $3 -N -L 27017:hades$2.es.its.nyu.edu:27017 yns207@hpc.nyu.edu
+    sshpass -p $1 autossh -M $3 -N -L 27017:hades$2.es.its.nyu.edu:27017 $4@hpc.nyu.edu
     echo "$?"
 }
 
