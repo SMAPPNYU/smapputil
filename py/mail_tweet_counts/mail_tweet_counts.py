249e92fe1e8b5f36946a4887461b9beed9f8a2e7
yvan <yvanscher@gmail.com>
2016-04-13 14:27:52 -0700

    got rid of mail_tweet_count output

--- py/mail_tweet_counts/mail_tweet_counts.py
+++ py/mail_tweet_counts/mail_tweet_counts.py
@@ -36,7 +36,6 @@ def parse_args(args):
     parser = argparse.ArgumentParser()
     parser.add_argument('-ho', '--host', dest='host', required=True, help='local hostname to map to')
     parser.add_argument('-p', '--port', dest='port', required=True, help='local port to map to the remoteport')
-    # parser.add_argument('-o', '--output', dest='output', required=True, help='Path to output file with servers and their collections.')
     parser.add_argument('-l', '--log', dest='log', default=expanduser('~/pylogs/list_collections.log'), help='This is the path to where your output log should be.')
     return parser.parse_args(args)
 
