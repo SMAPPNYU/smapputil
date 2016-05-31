f3371d9cb5864e8a5cb80cd4d5d7a7b2f68dcec4
yvan <yvanscher@gmail.com>
2016-05-31 17:28:29 -0400

    fixed dump script hotfix

--- py/archive_tools/dump_database.py
+++ py/archive_tools/dump_database.py
@@ -3,7 +3,6 @@ import sys
 import json
 import time
 import signal
-import psutil
 import logging
 import argparse
 import subprocess
@@ -11,16 +10,9 @@ import subprocess
 def dump_database(hostname, port, dbname, username, password, output_path):
     mongodump_string = "mongodump --host {}:{} -u {} -p {} -o {} -d {}".format(hostname, port, username, password, output_path, dbname)
     logger.info('trying to start: {}'.format(mongodump_string))
-    process = subprocess.Popen([mongodump_string], shell=True)
-    logger.info('process should be: {}'.format(process.pid))
-    proc = psutil.Process(process.pid)
-    logger.info('proc.status is {}'.format(proc.status()))
-    if proc.status() != psutil.STATUS_RUNNING and proc.status() != psutil.STATUS_SLEEPING:
-        logger.info('things are not right, trying to kill {}'.format(process.pid))
-        os.killpg(os.getpgid(int(process.pid)), signal.SIGTERM)
-    while proc.status() == psutil.STATUS_RUNNING or proc.status() == psutil.STATUS_SLEEPING:
-        logger.info('mongodumping with command: {}'.format(mongodump_string))
-        time.sleep(1)
+    #check_output is like popen but blocks to w8 for output
+    process = subprocess.check_output([mongodump_string], shell=True)
+    logger.info('finished: {}'.format(mongodump_string))
 
 def parse_args(args):
     parser = argparse.ArgumentParser()
