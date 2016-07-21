5a85edd6ed486c304e866c0990d91ef6af5f55c6
Roman <scher.roman@gmail.com>
2016-07-21 19:41:17 -0400

    New elastic tools script to report if dataset exists.
    - Improvements to es_index_dataset script

--- py/elastic_tools/es_index_dataset.py
+++ py/elastic_tools/es_index_dataset.py
@@ -4,52 +4,62 @@ import json
 import logging
 import argparse
 import datetime
+import pymongo
 from pysmap import SmappDataset
 from elasticsearch import Elasticsearch
 from elasticsearch import helpers
 
 currentdate = datetime.datetime.now().strftime("%Y-%m-%d %H:%M")
 
-def index_dataset(dataset_name, doc_type, db_host, db_port, db_user, db_pass, es_hosts):
+def index_dataset(dataset_name, doc_type, db_host, db_port, db_user, db_pass, es_host):
+
+	# Check if the dataset exists
+	mongo = pymongo.MongoClient(db_host, int(db_port))
+	database_names = mongo.database_names()
+	if dataset_name not in database_names:
+		print("Dataset {} does not exist.".format(dataset_name))
+		logging.info("Dataset {} does not exist.".format(dataset_name))
+		sys.exit(1)
+
+	es = Elasticsearch([es_host],
+						# find all es nodes in cluster
+						sniff_on_start=True,
+						# refresh nodes after a node fails to respond
+						sniff_on_connection_fail=True)
 
 	dataset = SmappDataset(['mongo', db_host, db_port, db_user, db_pass], collection_regex='(^data$|^tweets$|^tweets_\d+$)', database_regex='(^' + dataset_name + '$|^' + dataset_name + '_\d+$)')
 
-	es = Elasticsearch()
-	count = 0
-	actions = []
-	# Index documents in batches of 5000
+	print("Indexing dataset {}...".format(dataset_name))
+	logging.info("Indexing dataset...")
+	for success, info in helpers.parallel_bulk(es, genereate_actions(dataset, dataset_name, doc_type), thread_count=4, chunk_size=5000):
+		if not success:
+			print('A document failed to index:', info)
+			logging.info('A document failed to index:', info)
+
+def genereate_actions(dataset, dataset_name, doc_type):
 	for doc in dataset.get_collection_iterators():
-	# 	# Delete conflicting mongo ID & Irrelevant fields
-	# 	del doc['_id']
-	# 	del doc['random_number']
-	#     action = {
-	#         "_index": dataset_name,
-	#         "_type": doc_type,
-	#         "_id": doc['id_str'],
-	#         "_source": doc
-	#         "chunk_size": 5000
-	#     }
-	#     actions.append(action)
-	#     count += 1
-	#     if count == 5000:
-	#    		helpers.bulk(es, actions)
-	#    		actions = []
-	# # Index any remaining documents
-	# if len(actions) > 0:
-	# 	helpers.bulk(es, actions)
-	# 	actions = []
-		print(doc)
+		# Delete conflicting mongo ID & Irrelevant fields
+		del doc['_id']
+		del doc['random_number']
+		# Create action object for bulk indexing
+		yield {
+			'_op_type': 'create',
+			"_index": dataset_name.lower(),
+			"_type": doc_type,
+			"_id": doc['id_str'],
+			"_source": doc
+		}
 
 def parse_args(args):
 	parser = argparse.ArgumentParser()
 	parser.add_argument('-n', '--dataset-name', dest='dataset_name', required=True, help='an existing mongodb dataset to index in elasticsearch.')
 	parser.add_argument('-t', '--doc-type', dest='doc_type', required=True, help='the type of object in the dataset. e.g. tweet or user')
-	parser.add_argument('-h', '--db-host', dest='db_host', default='localhost', help='the host where mongodb is running')
+	parser.add_argument('-ho', '--db-host', dest='db_host', default='localhost', help='the host where mongodb is running')
 	parser.add_argument('-p', '--db-port', dest='db_port', default='49999', help='the port on the host where mongodb is running')
 	parser.add_argument('-u', '--db-user', dest='db_user', required=True, help='the username for the database to read from')
 	parser.add_argument('-w', '--db-pass', dest='db_pass', required=True, help='password for this user')
-	parser.add_argument('-eh', '--es-hosts', dest='es_hosts', required=True, help='list of elasticsearch data-node hosts, separated by spaces.')
-	parser.add_argument('-l', '--log', dest='log', default=os.path.expanduser('~/pylogs/' + os.path.basename(__file__) + parser.parse_args().dataset_name +  '.log'), help='This is the path to where your output log should be.')
+	parser.add_argument('-eh', '--es-host', dest='es_host', default='localhost', help='the hostname/IP of a host that is a part of the elasticsearch cluster.')
+	parser.add_argument('-l', '--log', dest='log', default=os.path.expanduser('~/pylogs/es_index_dataset_' + parser.parse_args().dataset_name +  '.log'), help='This is the path to where your output log should be.')
 	return parser.parse_args(args)
 
 if __name__ == '__main__':
@@ -58,8 +68,9 @@ if __name__ == '__main__':
 	logging.basicConfig(filename=args.log, level=logging.INFO, filemode='w')
 	logger = logging.getLogger(__name__)
 	logging.info('@timestamp: {}'.format(currentdate))
+	print('@timestamp: {}'.format(currentdate))
 
-	index_dataset(dataset_name, doc_type, db_host, db_port, db_user, db_pass, es_hosts)
+	index_dataset(args.dataset_name, args.doc_type, args.db_host, args.db_port, args.db_user, args.db_pass, args.es_host)
 
 '''
 author @Roman
