22c9604f842c1fe7a47007eb75d78ea2d0747531
yvan <yvanscher@gmail.com>
2016-08-22 09:13:56 -0400

    adedd script for query user ids dist

---
+++ py/query_twitter/query_user_id_distribution.py
@@ -0,0 +1,48 @@
+import os
+import sys
+import json
+import argparse
+import datetime
+import logging
+
+def query_distribution(output_file, input_file):
+    count_struct = {}
+    logger = logging.getLogger(__name__)
+
+    with open(input_file) as f:
+        for line in f:
+            try:
+                json_line = json.loads(line)
+            except ValueError as err:
+                logger.info('failed to read a json line : %s', err)
+
+            if 'smapp_original_user_id' in json_line and json_line['smapp_original_user_id'] not in count_struct:
+                count_struct[json_line['smapp_original_user_id']] = 1
+            elif 'smapp_original_user_id' in json_line:
+                count_struct[json_line['smapp_original_user_id']] += 1
+
+    with open(output_file, 'w') as filehandle:
+        val = 0
+        filehandle.write('{},{}'.format('id', 'smapp_tweet_count'))
+        filehandle.write('\n')
+        for key, value in count_struct.items():
+            val = val + value
+            filehandle.write('{},{}'.format(key, value))
+            filehandle.write('\n')
+        filehandle.write('all_users_total,{}'.format(val))
+
+def parse_args(args):
+    currentdate = datetime.datetime.now().strftime("%Y-%m-%d %H:%M")
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-i', '--input', dest='input', required=True, help='This is a path to your input.json, a [] list of twitter ids.')
+    parser.add_argument('-o', '--output', dest='output', required=True, help='This will be your output file, a {} json object showing original ids and twitter screen names.')
+    parser.add_argument('-l', '--log', dest='log', default=os.path.expanduser('~/pylogs/query_tweet_distribution'+currentdate+'.log'), help='This is the path to where your output log should be. Required')
+    return parser.parse_args(args)
+
+if __name__ == '__main__':
+    #setup parser for command line arguments
+    args = parse_args(sys.argv[1:])
+    #configure logs
+    logging.basicConfig(filename=args.log, level=logging.INFO)
+    # actually merge the bson files
+    query_distribution(os.path.expanduser(args.output), os.path.expanduser(args.input))
---
+++ py/test/test_query_user_ids_distribution.py
@@ -0,0 +1,49 @@
+import os
+import sys
+import csv
+import json
+import unittest
+sys.path.insert(0, os.path.dirname(os.path.abspath(__file__))+'/../query_twitter')
+
+import query_user_id_distribution
+
+class TestQueryDistribution(unittest.TestCase):
+
+    def setUp(self):
+        if os.path.exists(os.path.dirname(os.path.abspath(__file__))+'/../test/output.csv'):
+            os.remove(os.path.dirname(os.path.abspath(__file__))+'/../test/output.csv')
+
+    def tearDown(self):
+        if os.path.exists(os.path.dirname(os.path.abspath(__file__))+'/../test/output.csv'):
+            os.remove(os.path.dirname(os.path.abspath(__file__))+'/../test/output.csv')
+
+    def test_control(self):
+        self.assertTrue(True, 'Control test successful!')
+
+    def test_merge_json_parse_args(self):
+        args = query_user_id_distribution.parse_args(['-i', '~/data1.json', '-o', '~/data3.json'])
+        self.assertEqual(args.input,'~/data1.json')
+        self.assertEqual(args.output,'~/data3.json')
+
+    def test_query_user_ids_distribution(self):
+        # self.setUp()
+        args = query_user_id_distribution.parse_args(['-i', os.path.dirname(os.path.abspath(__file__))+'/../test/test_user_ids.json', '-o', os.path.dirname(os.path.abspath(__file__))+'/../test/output.csv'])
+        query_user_id_distribution.query_distribution(args.output, args.input)
+
+        with open(os.path.dirname(os.path.abspath(__file__))+'/../test/test_user_ids.json') as f:
+            original_total_count = sum(1 for _ in f)
+
+        with open(os.path.dirname(os.path.abspath(__file__))+'/../test/output.csv') as f:
+            reader = csv.DictReader(f)
+            for row in reader:
+                if row['id'] == 'all_users_total':
+                    new_total_count = int(row['smapp_tweet_count'])
+        self.assertEqual(original_total_count,new_total_count)
+        self.tearDown()
+
+if __name__ == '__main__':
+    unittest.main()
+
+'''
+author @yvan
+'''
