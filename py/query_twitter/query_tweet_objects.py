19e98ee7a2b776e9b3e18626ac62bf1003002210
yvan <yvanscher@gmail.com>
2016-11-02 14:12:14 -0400

    changes, still need to redo tweepy pool

--- py/query_twitter/query_tweet_objects.py
+++ py/query_twitter/query_tweet_objects.py
@@ -19,6 +19,8 @@ def twitter_query(output, file_input, auth_file):
     #query the tweets
     query_search_tweets(output, terms_list, auth_file)
 
+# problem here is that were using a method that doesnt exist in tweepy 2.3 we need to rewrite the tweepy pool
+# its long overdue.
 def query_search_tweets(output, terms_list, auth_file):
 
     logger = logging.getLogger(__name__)
@@ -40,7 +42,8 @@ def query_search_tweets(output, terms_list, auth_file):
             count = 0
             if not term == '':
                 try:
-                    for item in Cursor(api_pool.statuses_lookup, id=hundred_terms).items():
+                    for item in api_pool.statuses_lookup(hundred_terms):
+                        print(item)
                         logger.debug('tweet text: %s', item.text) 
                         count = count + 1
                         tweet_item = json.loads(json.dumps(item._json))
@@ -85,7 +88,7 @@ def parse_args(args):
     parser.add_argument('-i', '--input', dest='input', required=True, help='This is a path to your input.json, a [] list of twitter ids.')
     parser.add_argument('-o', '--output', dest='output', required=True, help='This will be your output file, a {} json object showing original ids and twitter screen names.')
     parser.add_argument('-a', '--auth', dest='auth', required=True, help='This is the path to your oauth.json file for twitter')
-    parser.add_argument('-l', '--log', dest='log', default=os.path.expanduser('~/pylogs/query_search_tweets'+currentdate+'.log'), help='This is the path to where your output log should be. Required')
+    parser.add_argument('-l', '--log', dest='log', default=os.path.expanduser('~/pylogs/query_tweet_objects'+currentdate+'.log'), help='This is the path to where your output log should be. Required')
     return parser.parse_args()
 
 if __name__ == '__main__':
