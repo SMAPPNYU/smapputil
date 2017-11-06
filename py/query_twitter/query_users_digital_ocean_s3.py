34d0681921685689438fec4950c24a1b2e4266eb
Leon Yin <leonyin@172-16-26-192.DYNAPOOL.NYU.EDU>
2017-11-06 18:19:17 -0500

    initial commit for create volume

--- py/query_twitter/query_users_digital_ocean_s3.py
+++ py/query_twitter/query_users_digital_ocean_s3.py
@@ -17,68 +17,19 @@ from tweepy import Cursor, TweepError
 
 s3_bucket = 'smapp-nyu'
 s3_path = 'query_machine_stage'
-droplet_reigon = 'nyc1'
 verbose = 1
 
 
 def get_ip_address():
+    '''
+    Gets the IP address of this machine.
+    '''
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     s.connect(("8.8.8.8", 80))
     return s.getsockname()[0]
 
-def create_and_attach_volume(context, size_gigabytes=800):
-    '''
-    TODO: remove the mount if it exists!
-    '''
-    commands = [
-        'sudo -S rm -rf /mnt/{VOL_NAME}',
-        "sudo -S mkfs.ext4 -F /dev/disk/by-id/scsi-0DO_Volume_{VOL_NAME}",
-        'sudo -S mkdir -p /mnt/{VOL_NAME}',
-        'sudo -S mount -o discard,defaults /dev/disk/by-id/scsi-0DO_Volume_{VOL_NAME} /mnt/{VOL_NAME}',
-        """sudo -S sh -c 'echo "/dev/disk/by-id/scsi-0DO_Volume_{VOL_NAME} /mnt/{VOL_NAME} ext4 defaults,nofail,discard 0 0">>/etc/fstab' """,        
-        'sudo -S chown {USER}:{USER} /mnt/{VOL_NAME}'
-    ]
-    
-    V = context['volume']
-    
-    try:
-        if verbose:
-            print("creating volume" + context['volume_name'])
-        V.create()
-    except Exception as e:
-        if verbose:
-            print("Issue attaching creating volume. {}".format(e))
-        pass
-    
-    time.sleep(5)
-    
-    try:
-        if verbose:
-            print("connecting volume" + context['volume_name'])
-        V.attach(droplet_id = context['droplet_id'], 
-                 region = context['droplet_reigon'])
-    except Exception as e:
-        if verbose:
-            print("Issue attaching volume. {}".format(e))
-#         return False
-        pass
-    
-    time.sleep(5)
-    
-    for command in commands:
-        command = command.format(VOL_NAME=context['volume_name'], 
-                                 USER=context['user'])
-        print(command)
-        p = Popen(command, stdin=PIPE, stderr=PIPE, shell=True, universal_newlines=True)
-        sudo_prompt = p.communicate(context['sudo_password'] + '\n')[1]
-        time.sleep(20)       
-    if verbose:
-        print("Volume {} configred!".format(context['volume_name']))
-   
-    return True
 
-
-def destroy(context):
+def destroy_droplet(context):
     '''
     This is where it ends :)
     '''
@@ -117,6 +68,9 @@ def detach_and_destroy_volume(context):
     
 
 def send_to_s3(context):
+    '''
+    Uses boto3 to send a the bzipped file to s3.
+    '''
     f_out = context['output_bz2']
     if verbose:
         print("Sending file to s3".format(f_out))
@@ -129,8 +83,12 @@ def send_to_s3(context):
         s3_dest = os.path.join('s3://' + context['s3_bucket'], context['s3_path'])
         print("Sent file to {}".format(s3_dest))
 
-
 def bzip(context):
+    '''
+    Bzips a file.
+    When this is successful, the original file will have disappeared.
+    When that happens this returns the new file name with the .bz2 extension.
+    '''
     f_out = context['output']
     command = ['/bin/bzip2', f_out]
     process = Popen(command, stdin=PIPE, stderr=PIPE)
@@ -140,7 +98,11 @@ def bzip(context):
         
     return f_out + '.bz2'
 
+
 def twitter_query(context):
+    '''
+    Gets user ids, and feeds them into a function to query twitter.
+    '''
     if verbose:
         print('Starting query!')
     
@@ -158,7 +120,9 @@ def twitter_query(context):
 
 
 def query_user_tweets(output, id_list, auth_file):
-
+    '''
+    queries twitter for users from id_list and authentication from auth_file.
+    '''
     logger = logging.getLogger(__name__)
 
     num_inputs_queried = 0
@@ -189,7 +153,12 @@ def query_user_tweets(output, id_list, auth_file):
         logger.info('number of inputs queried so far: %s', num_inputs_queried)
     write_fd.close()
 
+
 def get_id_list(file_input):
+    '''
+    opens list of user ids to query.
+    from the -i arg
+    '''
     logger = logging.getLogger(__name__)
     filename, file_extension = os.path.splitext(file_input)
     id_list = []
@@ -208,50 +177,59 @@ def get_id_list(file_input):
         logger.info('launching query for %s inputs', len(id_list))
     return id_list
 
-def parse_args(args):
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-i', '--input', dest='input', required=True, help='This is a path to your input.json, a [] list of twitter ids.')
-    parser.add_argument('-a', '--auth', dest='auth', required=True, help='This is the path to your oauth.json file for twitter')
-    parser.add_argument('-s', '--volsize', dest='volume_size_gbs', required=True, type=int, help='The size in gbs of the volume you want')
-    parser.add_argument('-d', '--debug', dest='debug', required=True,  type=int, help='debug')
-    return vars(parser.parse_args())
+
+def check_vol_attached(context):
+    '''
+    Checks to see if a digital ocean volume is attached to the machine.
+    Returns False if not, otherwise returns a 
+    '''
+    manager = digitalocean.Manager(token=context['token'])
+    
+    vols =  manager.get_all_volumes()
+    myvol = [v for v in vols if context['droplet_id'] in v.droplet_ids]
+    
+    if not myvol: # check if volume is attached.
+        return False
+    else:
+        return myvol[0]
+
 
 def build_context(args):
+    '''
+    This creates a dictionary of variables we'll be using throughout the script.
+    args are from parse_args
+    '''
     context = args
     currentdate = datetime.datetime.now().strftime("%Y-%m-%d")
-    
+    if 'token' not in context:
+        context['token'] = os.environ.get('DO_TOKEN')
+
     # digital ocean
-    context['token'] = os.environ.get('DO_TOKEN')
     manager = digitalocean.Manager(token=context['token'])
     my_droplets = manager.get_all_droplets()
     mydrop = [_ for _ in my_droplets if _.ip_address == get_ip_address()][0]
-    
-    context['droplet_id'] = mydrop.id
+
     context['droplet'] = mydrop
+    context['droplet_id'] = mydrop.id
     context['volume_name'] = mydrop.name + '-volume'
     context['volume_directory'] = '/mnt/' + context['volume_name']
-    context['droplet_reigon'] = droplet_reigon
-    context['volume'] = digitalocean.Volume(
-        token= context['token'],
-        name= context['volume_name'],
-        region= context['droplet_reigon'],
-        size_gigabytes= context['volume_size_gbs']
-    )
+
     output_base = 'query_respondants_' + currentdate + '_' + \
         context['input'].split('/')[-1].replace('.csv', '.json')
 
     # AWS s3
-    context['s3_bucket'] = s3_bucket
     context['s3_path'] = os.path.join(
-        s3_path, output_base + '.bz2'
+        context['s3_root'], output_base + '.bz2'
     )
     context['s3_log'] = os.path.join(
-        s3_path, output_base.replace('.json', '.log')
+        context['s3_root'], output_base.replace('.json', '.log')
     )
     
      # local stuff
     context['user'] = os.environ.get('USER')
-    context['sudo_password'] = os.environ.get('SUDO')
+    if 'sudo_password' not in context:
+        context['sudo_password'] = os.environ.get('SUDO')
+    
     context['output'] = os.path.join(
         context['volume_directory'], output_base
     )
@@ -261,26 +239,53 @@ def build_context(args):
     
     return context
 
+
+def parse_args(args):
+    '''
+    Which arguments we'll need
+    '''
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('-i', '--input', dest='input', required=True, help='This is a path to your input.json, a [] list of twitter ids.')
+    parser.add_argument('-a', '--auth', dest='auth', required=True, help='This is the path to your oauth.json file for twitter')
+    parser
+    parser.add_argument('-d', '--digital-ocean-token', dest='token', required=False, help='DO access token')
+    parser.add_argument('-b', '--s3-bucket', dest='s3_bucket', required=True, help='s3 bucket, ie s3://leonyin would be leonyin')
+    parser.add_argument('-r', '--s3-root', dest='s3_root', required=True, help='the path in the bucket.')
+    parser.add_argument('-s', '--sudo', dest='sudo_password', required=False, help='sudo pw for machine')
+
+    return vars(parser.parse_args())
+
+
 if __name__ == '__main__':
     '''
     Parse the input flags,
     create a context dict of all variables we're going to use,
-    create a connect a Digitial Ocean (DO) volume to store data,
+    Check to make sure the machine has a volume attached.
     start a log,
     query twitter,
     compress the returned json object from twitter,
     upload the compressed json and the log to s3
     destroy all files on the volume, detach, destroy.
     '''
-    #setup parser for command line arguments
     args = parse_args(sys.argv[1:])
     context = build_context(args)
-    if create_and_attach_volume(context) and not context['debug']:
-        logging.basicConfig(filename=context['log'], level=logging.INFO)
+    
+    logging.basicConfig(filename=context['log'], level=logging.INFO)
+    
+    context['volume'] = check_vol_attached(context)
+    if context['volume']:
+        clean_volume(context)
         twitter_query(context)
         context['output_bz2'] = bzip(context)
         send_to_s3(context)
         detach_and_destroy_volume(context)
-        destroy(context)
+        destroy_droplet(context)
 
+'''
+This script assumes a volume is attached to a digitalocean machine.
+It then queries twitter for all timelines for a given user id,
+and pools tokens.
 
+Leon Yin 2017-11-06
+'''
