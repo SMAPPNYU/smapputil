858b3bbec91954ca13c9e16ed5b504da1e46b34d
Leon Yin <leonyin@172-16-26-192.DYNAPOOL.NYU.EDU>
2017-11-06 18:43:19 -0500

    added default args for auth

--- py/query_twitter/query_users_digital_ocean_s3.py
+++ py/query_twitter/query_users_digital_ocean_s3.py
@@ -252,7 +252,7 @@ def parse_args(args):
     parser.add_argument('-d', '--digital-ocean-token', dest='token', required=False, help='DO access token', const=1, nargs='?', default=False)
     parser.add_argument('-b', '--s3-bucket', dest='s3_bucket', required=True, help='s3 bucket, ie s3://leonyin would be leonyin')
     parser.add_argument('-r', '--s3-root', dest='s3_root', required=True, help='the path in the bucket.')
-    parser.add_argument('-s', '--sudo', dest='sudo_password', nargs='?, default=False, help='sudo pw for machine')
+    parser.add_argument('-s', '--sudo', dest='sudo_password', nargs='?', default=False, help='sudo pw for machine')
 
     return vars(parser.parse_args())
 
