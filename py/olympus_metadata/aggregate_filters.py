cd20a2e89d09e8ea8beb9978300f5b348789a049
Nicole Baram <nhb228@nyu.edu>
2017-11-09 19:09:22 -0500

    changes to aggregate filters file to do more error checking and convert user ids to user names

--- py/olympus_metadata/aggregate_filters.py
+++ py/olympus_metadata/aggregate_filters.py
@@ -1,4 +1,5 @@
 import os
+import sys
 import re
 import json
 import glob
@@ -8,20 +9,11 @@ import subprocess
 from shutil import copyfile
 
 import pandas as pd
+import tweepy
+from tweepy.error import TweepError
 
 
-today = datetime.datetime.now().strftime('%Y_%m_%d')
-
-root = '/scratch/olympus/filter_metadata/'
-output_file = os.path.join(root, 'filter.csv')
-archive_file = os.path.join(root, 'archive/filter_{}.csv'.format(today))
-
-gdrive = ('gdrive:SMaPP_2017/SMAPP_ALL_MEMBERS/'
-          'Documentation/Twitter_Collection_Terms/')                          
-gdrive_archive = os.path.join(gdrive, 'z_Archive')
-
-cols_ignore = ['_id', 'filter_type', 'active', 'date_removed', 'collection']
-
+from config import *
 
 def convert_size(size_bytes):
     '''
@@ -33,13 +25,13 @@ def convert_size(size_bytes):
     i = int(math.floor(math.log(size_bytes, 1024)))
     p = math.pow(1024, i)
     s = round(size_bytes / p, 2)
-    
+
     return "%s %s" % (s, size_name[i])
 
 
 def return_size(f):
     '''
-    Gets the size of tweet files in aggregate and of the latest file.    
+    Gets the size of tweet files in aggregate and of the latest file.
     '''
     # creates a regular expression for tweet files.
     regex = f.split('/filters/')[0] + '/data/*'
@@ -51,17 +43,17 @@ def return_size(f):
     bytes_human = convert_size(bytes_)
     avg_size = bytes_ / num_files
     avg_size_human = convert_size(avg_size)
-    
+
     # gets the latest file and the date it was updated.
     newest = max(glob.iglob(regex), key=os.path.getctime)
-    
+
     latest_filedate = datetime.datetime.fromtimestamp(
         os.path.getctime(newest)
     )
-    
+
     latest_bytes = os.stat(newest).st_size
     latest_bytes_human = convert_size(latest_bytes)
-    
+
     row = dict(
         bytes_ = bytes_,
         bytes_human = bytes_human,
@@ -73,7 +65,7 @@ def return_size(f):
         latest_bytes = latest_bytes,
         latest_bytes_human = latest_bytes_human
     )
-    
+
     return row
 
 def append_collection(t, f):
@@ -81,11 +73,11 @@ def append_collection(t, f):
     Gets metadata about a filter.json file.
     '''
     t['collection'] = f.split('/')[3]
-    
+
     row = return_size(f)
-    
+
     z = {**t, **row}
-    
+
     return z
 
 
@@ -93,25 +85,25 @@ def clean_dates(d):
     '''
     Some of these files have dates of different formats.
     This script makes them the same!
-    
+
     input: d is a dictionary of filter metadata
     '''
     if isinstance(d['date_added'], dict):
         d['date_added'] = datetime.datetime.fromtimestamp(
                   d["date_added"]["$date"]/1000.0)
     elif d['date_added'] is None:
-        d['date_added'] = datetime.datetime(1950, 1, 1)  
+        d['date_added'] = datetime.datetime(1950, 1, 1)
     elif not isinstance(d['date_added'], datetime.datetime):
         try:
             d['date_added'] = datetime.datetime.strptime(
                 d["date_added"], "%a %b %d %H:%M:%S %z %Y")
         except:
             d['date_added'] = datetime.datetime.strptime(
-                d["date_added"].split('T')[0], "%Y-%m-%d")                
-        
+                d["date_added"].split('T')[0], "%Y-%m-%d")
+
     d['date_added'] = datetime.datetime.strftime(
             d['date_added'], '%Y-%m-%d')
-    
+
     return d
 
 
@@ -128,11 +120,58 @@ def rclone(src, dest):
 
     return result.communicate()
 
+def check_connection():
+    '''
+    check tweepy authentication by calling "me"!
+
+    calls global api
+    '''
+    try:
+        api.me()
+        return True
+    except(TweepError):
+        return False
+
+def get_username(user_id):
+    '''
+    gets users username with Tweepy
+    calls global api variable
+    '''
+    try:
+        u = api.get_user(user_id)
+        return u.name
+    except(TweepError):
+        return user_id
+
+
+def update_user_ids(user_ids):
+    '''
+    checks if file exists, if not creates a new files
+    checks if each user's id already exists, and if it doesn't,
+        calls tweepy api to get username for each id
+    '''
+    if os.path.exists(user_lookup_path):
+        with open(user_lookup_path, 'r') as file:
+            user_lookup = json.loads(file.read())
+
+    else:
+        user_lookup = {}
+    for user_id in user_ids:
+        if not user_id in user_lookup:
+            user_lookup[user_id] = get_username(user_id)
+
+    return user_lookup
+
 
 def main():
     '''
     This is the main function that utilizes the functions above.
     '''
+
+    if not check_connection():
+        sys.exit("authentication error")
+
+
     # get the paths of all filter metadata files.
     files = glob.glob('/scratch/olympus/*/filters/*.json')
 
@@ -144,27 +183,37 @@ def main():
         tweets = []
         for line in open(f, 'rb'):
             tweets.append(clean_dates(json.loads(line.decode('utf-8'))))
-           
+
         # get metadata about size and quantity.
         d = [append_collection(t, f) for t in tweets \
-             if t['filter_type'] == 'track']
+             if t['filter_type'] in ['track', 'follow']]
         d_ += d
 
     # put the contents into a pandas dataframe.
     df = pd.DataFrame(d_)
     df.sort_values(by='collection', inplace=True)
-    
+
+
+    user_ids = df[df["filter_type"] == "follow"]['value'].unique()
+
+    user_lookup = update_user_ids(user_ids)
+
+    with open(user_lookup_path, 'w') as f:
+        f.write(json.dumps(user_lookup))
+
+    df['value'].replace(user_lookup, inplace=True)
+
     # These are the columns we want!
     cols = ['collection'] + [c for c in df.columns if c not in cols_ignore]
 
     # filter out columns we don't want, and write the results to a csv.
     df[cols].to_csv(output_file, index=False)
-    
+
     # copy the file into the archive.
     copyfile(output_file, archive_file)
-   
+
     # send the files to Google Drive.
     rclone(output_file, gdrive)
     rclone(archive_file, gdrive_archive)
-    
+
 main()
